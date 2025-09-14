55f46c366e3eaa00b2685d79ed8fe7c784405a32
Mohamed Tamer <mohamed.tamerrr79@gmail.com>
2025-09-14 21:18:40 +0300

    make the body scrollable

--- lib/widgets/dash_board_desktop_layout.dart
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -22,20 +22,36 @@ class DashBoardDesktopLayout extends StatelessWidget {
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
         Expanded(
-          flex: 2,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: AllExpensesAndQuickInvoice(),
-          ),
-        ),
-        SizedBox(width: 24),
-        Expanded(
-          child: Column(
-            children: [
-              SizedBox(height: 40),
-              MyCardsAndTransctionHistorySection(),
-              SizedBox(height: 24),
-              Expanded(child: IncomeSection()),
+          flex: 3,
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 40.0,
+                        ),
+                        child: AllExpensesAndQuickInvoice(),
+                      ),
+                    ),
+                    SizedBox(width: 24),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          SizedBox(height: 40),
+                          MyCardsAndTransctionHistorySection(),
+                          SizedBox(height: 24),
+                          Expanded(child: IncomeSection()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
